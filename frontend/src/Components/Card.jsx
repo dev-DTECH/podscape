47fa6321bb51a733cde9c616daede9c93638dab6
dev-DTECH <debdeep.dt@gmail.com>
2023-08-13 19:54:48 +0530

    Added newRelease route and intregated it

--- frontend/src/Components/Card.jsx
+++ frontend/src/Components/Card.jsx
@@ -1,47 +1,53 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import "../App.css";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import { ToastContainer, toast } from "react-toastify";
+import {ToastContainer, toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+const API_BASE_URL = "http://127.0.0.1:8000"
+
 const Card = (props) => {
-  const [isFavorite, setIsFavorite] = useState(props.favourite);
+    const [isFavorite, setIsFavorite] = useState(props.favourite);
 
-  const handleAddToFavorites = () => {
-    toast.success("Added to favorites");
-  };
-  const handleRemoveFromFavorites = () => {
-    // ...other code
-    toast.error("Removed from favorites!");
-  };
+    const handleAddToFavorites = () => {
+        toast.success("Added to favorites");
+    };
+    const handleRemoveFromFavorites = () => {
+        // ...other code
+        toast.error("Removed from favorites!");
+    };
 
-  const handleClick = () => {
-    
-    setIsFavorite(!isFavorite);
-    
-    if (!isFavorite) {
-      handleAddToFavorites();
-    } else {
-      handleRemoveFromFavorites();
-    }
-  };
+    const handleClick = () => {
 
+        setIsFavorite(!isFavorite);
 
-  return (
-    <div className="card">
-      <img src={props.pic} alt="pic1" className="card-pic" />
-      <h3 className="category">{props.category}</h3>
-      <div className="wrap">
-        <h2 className="card-header">{props.heading}</h2>
-        <FavoriteIcon
-          className="love"
-          style={{ fill: isFavorite ? "#8952ed" : "rgb(200, 200, 200)" }}
-          onClick={handleClick}
-        />
-      </div>
-      <ToastContainer position="bottom-right" />
-    </div>
-  );
+        if (!isFavorite) {
+            handleAddToFavorites();
+        } else {
+            handleRemoveFromFavorites();
+        }
+    };
+    console.log(props.pid);
+
+    return (
+        <div className="card">
+            <a href={API_BASE_URL + "/player/" + props.pid}>
+                <img src={API_BASE_URL + "/thumbnail/" + props.pid} alt="pic1" className="card-pic"/>
+            </a>
+
+                <h3 className="category">{props.category}</h3>
+                <div className="wrap">
+                    <h2 className="card-header">{props.heading}</h2>
+                    <FavoriteIcon
+                        className="love"
+                        style={{fill: isFavorite ? "#8952ed" : "rgb(200, 200, 200)"}}
+                        onClick={handleClick}
+                    />
+                </div>
+            <ToastContainer position="bottom-right"/>
+        </div>
+    )
+        ;
 };
 
 export default Card;
--- frontend/src/Components/newRelease.jsx
+++ frontend/src/Components/newRelease.jsx
@@ -2,14 +2,34 @@ import React from 'react';
 import "../App.css";
 import Card from "./Card";
 import pic1 from "../images/pic1.jpeg";
+import axios from "axios";
+
 const newRelease = () => {
-  return (
-    <>
-      <h3 className='new-release'>New Releases</h3>
-      <Card pic ={pic1} favourite ={true} category = {"Horror and Adventure"} heading = {"Sunday Suspense"}/>
-      
-    </>
-  )
+    const [podcast, setPodcast] = React.useState([]);
+
+    console.log(process.env.NODE_ENV)
+    React.useEffect(() => {
+        axios.get("http://127.0.0.1:8000/newRelease/").then((res) => {
+            console.log(res.data?.podcast);
+            setPodcast(res.data?.podcast);
+        });
+    },[]);
+    if (!podcast) {
+        return <div>loading...</div>;
+    }
+    return (
+        <>
+            <h3 className='new-release'>New Releases</h3>
+            {
+                podcast.map((podcast) => <Card key={podcast.pid} pid={podcast.pid} favourite={true} category={podcast.category}
+                                               heading={podcast.title}/>
+                )
+            }
+
+            {/*<Card pic={pic1} favourite={true} category={"Horror and Adventure"} heading={"Sunday Suspense"}/>*/}
+
+        </>
+    )
 }
 
 export default newRelease
